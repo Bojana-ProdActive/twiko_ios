a48bb8bb550241103da93ff9b4029550683b7cc5
Goran Tokovic <goran.tokovic@digitalatrium.rs>
2021-05-20 16:29:04 +0200

    Created NDBluetoothPump framework project

---
+++ NDBluetoothPump/NDBluetoothPump/NDBluetoothPump.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// NeuroDerm NDBluetoothPump
+// Copyright (c) 2021 NeuroDerm
+// All Rights Reserved Worldwide
+// Proprietary and Confidential - Not for Distribution
+// Written by NeuroDerm.
+//
+// NDBluetoothPump.h
+//
+// AUTHOR IDENTITY:
+//        Goran Tokovic        20.5.21.
+//
+////////////////////////////////////////////////////////////////////////////////
+#import <Foundation/Foundation.h>
+////////////////////////////////////////////////////////////////////////////////
+
+//! Project version number for NDBluetoothPump.
+FOUNDATION_EXPORT double NDBluetoothPumpVersionNumber;
+
+//! Project version string for NDBluetoothPump.
+FOUNDATION_EXPORT const unsigned char NDBluetoothPumpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDBluetoothPump/PublicHeader.h>
